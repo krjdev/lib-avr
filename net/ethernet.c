bcd86480f631d51a679fe0caedcc959c59a0c205
Johannes Krottmayer <krjdev@gmail.com>
2019-01-30 22:19:04 +0100

    release version 0.2.0.0

--- net/ethernet.c
+++ net/ethernet.c
@@ -1,13 +1,13 @@
 /**
  *
  * File Name: ethernet.c
- * Title    : Ethernet (IEEE 802.3) definitions and helper source
+ * Title    : Ethernet (IEEE 802.3) definitions and helper functions source
  * Project  : lib-avr
- * Author   : Copyright (C) 2018 Johannes Krottmayer <krjdev@gmail.com>
+ * Author   : Copyright (C) 2018-2019 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2018-09-24
- * Modified : 
+ * Modified : 2019-01-30
  * Revised  : 
- * Version  : 0.1.0.0
+ * Version  : 0.2.0.0
  * License  : ISC (see file LICENSE.txt)
  * Target   : Atmel AVR Series
  *
@@ -26,151 +26,97 @@
 
 #include "ethernet.h"
 
-mac_addr_t *ethernet_str_to_mac(const char *str)
+int ethernet_atom(const char *str, mac_addr_t *mac)
 {
-    mac_addr_t *p;
     int i = 0;
     char tmp[3];
     
     if (!str)
-        return NULL;
-    
-    if (strlen(str) != 17)
-        return NULL;
-    
-    p = (mac_addr_t *) malloc(sizeof(mac_addr_t));
-    
-    if (!p)
-        return NULL;
-    
-    memcpy(&tmp[0], &str[i], 2);
-    tmp[2] = '\0';
-    sscanf(tmp, "%hhx", &p->ma_byte0);
-    i += 3;
-    memcpy(&tmp[0], &str[i], 2);
-    tmp[2] = '\0';
-    sscanf(tmp, "%hhx", &p->ma_byte1);
-    i += 3;
-    memcpy(&tmp[0], &str[i], 2);
-    tmp[2] = '\0';
-    sscanf(tmp, "%hhx", &p->ma_byte2);
-    i += 3;
-    memcpy(tmp, &str[i], 2);
-    tmp[2] = '\0';
-    sscanf(tmp, "%hhx", &p->ma_byte3);
-    i += 3;
-    memcpy(tmp, &str[i], 2);
-    tmp[2] = '\0';
-    sscanf(tmp, "%hhx", &p->ma_byte4);
-    i += 3;
-    memcpy(tmp, &str[i], 2);
-    tmp[2] = '\0';
-    sscanf(tmp, "%hhx", &p->ma_byte5);
-    return p;
-}
-
-char *ethernet_mac_to_str(mac_addr_t *mac)
-{
-    char *p;
+        return -1;
     
     if (!mac)
-        return NULL;
+        return -1;
     
-    p = (char *) malloc(18);
+    if (strlen(str) != 17)
+        return -1;
     
-    if (!p)
-        return NULL;
+    memcpy(&tmp[0], &str[i], 2);
+    tmp[2] = '\0';
+    sscanf(tmp, "%hhx", &mac->ma_byte0);
+    i += 3;
+    memcpy(&tmp[0], &str[i], 2);
+    tmp[2] = '\0';
+    sscanf(tmp, "%hhx", &mac->ma_byte1);
+    i += 3;
+    memcpy(&tmp[0], &str[i], 2);
+    tmp[2] = '\0';
+    sscanf(tmp, "%hhx", &mac->ma_byte2);
+    i += 3;
+    memcpy(tmp, &str[i], 2);
+    tmp[2] = '\0';
+    sscanf(tmp, "%hhx", &mac->ma_byte3);
+    i += 3;
+    memcpy(tmp, &str[i], 2);
+    tmp[2] = '\0';
+    sscanf(tmp, "%hhx", &mac->ma_byte4);
+    i += 3;
+    memcpy(tmp, &str[i], 2);
+    tmp[2] = '\0';
+    sscanf(tmp, "%hhx", &mac->ma_byte5);
+    return 0;
+}
+
+int ethernet_mtoa(mac_addr_t *mac, char *str)
+{
+    if (!mac)
+        return -1;
     
-    sprintf(p, "%02X:%02X:%02X:%02X:%02X:%02X", mac->ma_byte0, mac->ma_byte1, 
+    if (!str)
+        return -1;
+    
+    sprintf(str, "%02X:%02X:%02X:%02X:%02X:%02X", mac->ma_byte0, mac->ma_byte1, 
             mac->ma_byte2, mac->ma_byte3, mac->ma_byte4, mac->ma_byte5);
-    return p;
-}
-
-int ethernet_frame_set_dst(eth_frame_t *frame, const char *addr)
-{
-    mac_addr_t *p;
-    
-    if (!frame)
-        return -1;
-    
-    if (!addr)
-        return -1;
-    
-    p = ethernet_str_to_mac(addr);
-    
-    if (!p)
-        return -1;
-    
-    frame->ef_dst.ma_byte0 = p->ma_byte0;
-    frame->ef_dst.ma_byte1 = p->ma_byte1;
-    frame->ef_dst.ma_byte2 = p->ma_byte2;
-    frame->ef_dst.ma_byte3 = p->ma_byte3;
-    frame->ef_dst.ma_byte4 = p->ma_byte4;
-    frame->ef_dst.ma_byte5 = p->ma_byte5;
-    free(p);
     return 0;
 }
 
-int ethernet_frame_get_dst(eth_frame_t *frame, char **str)
+int ethernet_mcpy(mac_addr_t *dst, mac_addr_t *src)
 {
-    char *p;
-    
-    if (!frame)
+    if (!dst)
         return -1;
     
-    if (!str)
+    if (!src)
         return -1;
     
-    p = ethernet_mac_to_str(&frame->ef_dst);
-    
-    if (!p)
-        return -1;
-    
-    (*str) = p;
+    dst->ma_byte0 = src->ma_byte0;
+    dst->ma_byte1 = src->ma_byte1;
+    dst->ma_byte2 = src->ma_byte2;
+    dst->ma_byte3 = src->ma_byte3;
+    dst->ma_byte4 = src->ma_byte4;
+    dst->ma_byte5 = src->ma_byte5;
     return 0;
 }
 
-int ethernet_frame_set_src(eth_frame_t *frame, const char *addr)
+int ethernet_frame_set_dst(eth_frame_t *frame, mac_addr_t *mac)
 {
-    mac_addr_t *p;
-    
     if (!frame)
         return -1;
     
-    if (!addr)
+    if (!mac)
         return -1;
     
-    p = ethernet_str_to_mac(addr);
-    
-    if (!p)
-        return -1;
-    
-    frame->ef_src.ma_byte0 = p->ma_byte0;
-    frame->ef_src.ma_byte1 = p->ma_byte1;
-    frame->ef_src.ma_byte2 = p->ma_byte2;
-    frame->ef_src.ma_byte3 = p->ma_byte3;
-    frame->ef_src.ma_byte4 = p->ma_byte4;
-    frame->ef_src.ma_byte5 = p->ma_byte5;
-    free(p);
+    ethernet_mcpy(&frame->ef_dst, mac);
     return 0;
 }
 
-int ethernet_frame_get_src(eth_frame_t *frame, char **str)
+int ethernet_frame_set_src(eth_frame_t *frame, mac_addr_t *mac)
 {
-    char *p;
-    
     if (!frame)
         return -1;
     
-    if (!str)
+    if (!mac)
         return -1;
     
-    p = ethernet_mac_to_str(&frame->ef_src);
-    
-    if (!p)
-        return -1;
-    
-    (*str) = p;
+    ethernet_mcpy(&frame->ef_src, mac);
     return 0;
 }
 
@@ -180,6 +126,46 @@ int ethernet_frame_set_type(eth_frame_t *frame, uint16_t type)
         return -1;
     
     frame->ef_type = type;
+    return 0;
+}
+
+int ethernet_frame_set_payload(eth_frame_t *frame, uint8_t *buf, int len)
+{
+    if (!frame)
+        return -1;
+    
+    if ((len > 0 ) && (buf != NULL)) {
+        frame->ef_payload_buf = buf;
+        frame->ef_payload_len = len;
+    } else {
+        frame->ef_payload_buf = NULL;
+        frame->ef_payload_len = 0;
+    }
+    
+    return 0;
+}
+
+int ethernet_frame_get_dst(eth_frame_t *frame, mac_addr_t *mac)
+{
+    if (!frame)
+        return -1;
+    
+    if (!mac)
+        return -1;
+    
+    ethernet_mcpy(mac, &frame->ef_dst);
+    return 0;
+}
+
+int ethernet_frame_get_src(eth_frame_t *frame, mac_addr_t *mac)
+{
+    if (!frame)
+        return -1;
+    
+    if (!mac)
+        return -1;
+
+    ethernet_mcpy(mac, &frame->ef_src);
     return 0;
 }
 
@@ -195,144 +181,126 @@ int ethernet_frame_get_type(eth_frame_t *frame, uint16_t *type)
     return 0;
 }
 
-int ethernet_frame_set_payload(eth_frame_t *frame, uint8_t *buffer, int length)
+int ethernet_frame_get_payload(eth_frame_t *frame, uint8_t **buf, int *len)
 {
     if (!frame)
         return -1;
     
-    if (!buffer)
+    if (!buf)
         return -1;
     
-    if (length < 1)
+    if (!len)
         return -1;
     
-    frame->ef_payload_buf = buffer;
-    frame->ef_payload_len = length;
+    (*buf) = frame->ef_payload_buf;
+    (*len) = frame->ef_payload_len;
     return 0;
 }
 
-int ethernet_frame_get_payload(eth_frame_t *frame, uint8_t **buffer, int *length)
+int ethernet_frame_get_len(eth_frame_t *frame)
 {
     if (!frame)
         return -1;
     
-    if (!buffer)
-        return -1;
-    
-    if (!length)
-        return -1;
-    
-    (*buffer) = frame->ef_payload_buf;
-    (*length) = frame->ef_payload_len;
-    return 0;
+    return (14 + frame->ef_payload_len);
 }
 
-int ethernet_buffer_to_frame(uint8_t *buffer, int length, eth_frame_t **frame)
+int ethernet_frame_free(eth_frame_t *frame)
 {
-    eth_frame_t *p_f = NULL;
-    uint8_t *p = NULL;
+    if (!frame)
+        return -1;
+    
+    if (frame->ef_payload_len > 0)
+        free(frame->ef_payload_buf);
+}
+
+int ethernet_buf_to_frm(uint8_t *buf, int len, eth_frame_t *frame)
+{
     uint16_t type;
+    uint8_t *p;
     int i = 0;
     
-    if (!buffer)
+    if (!buf)
+        return -1;
+    
+    if (len < 14)
         return -1;
     
     if (!frame)
         return -1;
     
-    p_f = (eth_frame_t *) malloc(sizeof(eth_frame_t));
-    
-    if (!p_f)
-        return -1;
-    
-    p = (uint8_t *) malloc(length - 14);
-    
-    if (!p) {
-        free(p_f);
-        return -1;
-    }
-    
     /* Destination address */
-    p_f->ef_dst.ma_byte0 = buffer[i++];
-    p_f->ef_dst.ma_byte1 = buffer[i++];
-    p_f->ef_dst.ma_byte2 = buffer[i++];
-    p_f->ef_dst.ma_byte3 = buffer[i++];
-    p_f->ef_dst.ma_byte4 = buffer[i++];
-    p_f->ef_dst.ma_byte5 = buffer[i++];
+    frame->ef_dst.ma_byte0 = buf[i++];
+    frame->ef_dst.ma_byte1 = buf[i++];
+    frame->ef_dst.ma_byte2 = buf[i++];
+    frame->ef_dst.ma_byte3 = buf[i++];
+    frame->ef_dst.ma_byte4 = buf[i++];
+    frame->ef_dst.ma_byte5 = buf[i++];
     
     /* Source address */
-    p_f->ef_src.ma_byte0 = buffer[i++];
-    p_f->ef_src.ma_byte1 = buffer[i++];
-    p_f->ef_src.ma_byte2 = buffer[i++];
-    p_f->ef_src.ma_byte3 = buffer[i++];
-    p_f->ef_src.ma_byte4 = buffer[i++];
-    p_f->ef_src.ma_byte5 = buffer[i++];
+    frame->ef_src.ma_byte0 = buf[i++];
+    frame->ef_src.ma_byte1 = buf[i++];
+    frame->ef_src.ma_byte2 = buf[i++];
+    frame->ef_src.ma_byte3 = buf[i++];
+    frame->ef_src.ma_byte4 = buf[i++];
+    frame->ef_src.ma_byte5 = buf[i++];
     
     /* Type */
-    type = (buffer[i++] << 8);
-    type |= buffer[i++];
-    p_f->ef_type = type;
+    type = (buf[i++] << 8);
+    type |= buf[i++];
+    frame->ef_type = type;
     
     /* Payload/Data */
-    p_f->ef_payload_buf = p;
-    p_f->ef_payload_len = (length - 14);
-    memcpy(p_f->ef_payload_buf, &buffer[i], p_f->ef_payload_len);
+    if (len > 14) {
+        p = (uint8_t *) malloc(len - 14);
+        
+        if (!p)
+            return -1;
     
-    (*frame) = p_f;
+        frame->ef_payload_buf = p;
+        frame->ef_payload_len = (len - 14);
+        memcpy(frame->ef_payload_buf, &buf[i], frame->ef_payload_len);
+    }
+    
     return 0;
 }
 
-int ethernet_frame_to_buffer(eth_frame_t *frame, uint8_t **buffer, int *length)
+int ethernet_frm_to_buf(eth_frame_t *frame, uint8_t *buf)
 {
-    uint8_t *p;
-    int len;
     int i = 0;
     int j, k;
     
     if (!frame)
         return -1;
     
-    if (!buffer)
-        return -1;
-    
-    if (!length)
-        return -1;
-    
-    len = frame->ef_payload_len;
-    len += sizeof(frame->ef_dst);
-    len += sizeof(frame->ef_src);
-    len += sizeof(frame->ef_type);
-
-    p = (uint8_t *) malloc(len);
-    
-    if (!p)
+    if (!buf)
         return -1;
     
     /* Destination address */
-    p[i++] = frame->ef_dst.ma_byte0;
-    p[i++] = frame->ef_dst.ma_byte1;
-    p[i++] = frame->ef_dst.ma_byte2;
-    p[i++] = frame->ef_dst.ma_byte3;
-    p[i++] = frame->ef_dst.ma_byte4;
-    p[i++] = frame->ef_dst.ma_byte5;
+    buf[i++] = frame->ef_dst.ma_byte0;
+    buf[i++] = frame->ef_dst.ma_byte1;
+    buf[i++] = frame->ef_dst.ma_byte2;
+    buf[i++] = frame->ef_dst.ma_byte3;
+    buf[i++] = frame->ef_dst.ma_byte4;
+    buf[i++] = frame->ef_dst.ma_byte5;
     
     /* Source address */
-    p[i++] = frame->ef_src.ma_byte0;
-    p[i++] = frame->ef_src.ma_byte1;
-    p[i++] = frame->ef_src.ma_byte2;
-    p[i++] = frame->ef_src.ma_byte3;
-    p[i++] = frame->ef_src.ma_byte4;
-    p[i++] = frame->ef_src.ma_byte5;
+    buf[i++] = frame->ef_src.ma_byte0;
+    buf[i++] = frame->ef_src.ma_byte1;
+    buf[i++] = frame->ef_src.ma_byte2;
+    buf[i++] = frame->ef_src.ma_byte3;
+    buf[i++] = frame->ef_src.ma_byte4;
+    buf[i++] = frame->ef_src.ma_byte5;
 
     /* Type */
-    p[i++] = HIGH16(frame->ef_type);
-    p[i++] = LOW16(frame->ef_type);
+    buf[i++] = HIGH16(frame->ef_type);
+    buf[i++] = LOW16(frame->ef_type);
     
     /* Payload/Data */
-    for (j = i, k = 0; j < (frame->ef_payload_len + i); j++, k++)
-        p[j] = frame->ef_payload_buf[k];
+    if (frame->ef_payload_len > 0) {
+        for (j = i, k = 0; j < (frame->ef_payload_len + i); j++, k++)
+            buf[j] = frame->ef_payload_buf[k];
+    }
     
-    (*buffer) = p;
-    (*length) = len;
     return 0;
 }
--- net/ethernet.h
+++ net/ethernet.h
@@ -1,13 +1,13 @@
 /**
  *
  * File Name: ethernet.h
- * Title    : Ethernet (IEEE 802.3) definitions and helper header
+ * Title    : Ethernet (IEEE 802.3) definitions and helper functions header
  * Project  : lib-avr
- * Author   : Copyright (C) 2018 Johannes Krottmayer <krjdev@gmail.com>
+ * Author   : Copyright (C) 2018-2019 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2018-09-24
- * Modified : 
+ * Modified : 2019-01-30
  * Revised  : 
- * Version  : 0.1.0.0
+ * Version  : 0.2.0.0
  * License  : ISC (see file LICENSE.txt)
  * Target   : Atmel AVR Series
  *
@@ -21,6 +21,9 @@
 #define LIBAVR_NET_ETHERNET_H
 
 #include <stdint.h>
+
+#define ETHERNET_TYPE_IPV4  0x0800
+#define ETHERNET_TYPE_ARP   0x0806
 
 typedef struct mac_addr {
     uint8_t ma_byte0;
@@ -39,17 +42,20 @@ typedef struct eth_frame {
     int ef_payload_len;
 } eth_frame_t;
 
-extern mac_addr_t *ethernet_str_to_mac(const char *str);
-extern char *ethernet_mac_to_str(mac_addr_t *mac);
-extern int ethernet_frame_set_dst(eth_frame_t *frame, const char *addr);
-extern int ethernet_frame_get_dst(eth_frame_t *frame, char **str);
-extern int ethernet_frame_set_src(eth_frame_t *frame, const char *addr);
-extern int ethernet_frame_get_src(eth_frame_t *frame, char **str);
+extern int ethernet_atom(const char *str, mac_addr_t *mac);
+extern int ethernet_mtoa(mac_addr_t *mac, char *str);
+extern int ethernet_mcpy(mac_addr_t *dst, mac_addr_t *src);
+extern int ethernet_frame_set_dst(eth_frame_t *frame, mac_addr_t *mac);
+extern int ethernet_frame_set_src(eth_frame_t *frame, mac_addr_t *mac);
 extern int ethernet_frame_set_type(eth_frame_t *frame, uint16_t type);
+extern int ethernet_frame_set_payload(eth_frame_t *frame, uint8_t *buf, int len);
+extern int ethernet_frame_get_dst(eth_frame_t *frame, mac_addr_t *mac);
+extern int ethernet_frame_get_src(eth_frame_t *frame, mac_addr_t *mac);
 extern int ethernet_frame_get_type(eth_frame_t *frame, uint16_t *type);
-extern int ethernet_frame_set_payload(eth_frame_t *frame, uint8_t *buffer, int length);
-extern int ethernet_frame_get_payload(eth_frame_t *frame, uint8_t **buffer, int *length);
-extern int ethernet_buffer_to_frame(uint8_t *buffer, int length, eth_frame_t **frame);
-extern int ethernet_frame_to_buffer(eth_frame_t *frame, uint8_t **buffer, int *length);
+extern int ethernet_frame_get_payload(eth_frame_t *frame, uint8_t **buf, int *len);
+extern int ethernet_frame_get_len(eth_frame_t *frame);
+extern int ethernet_frame_free(eth_frame_t *frame);
+extern int ethernet_buf_to_frm(uint8_t *buf, int len, eth_frame_t *frame);
+extern int ethernet_frm_to_buf(eth_frame_t *frame, uint8_t *buf);
 
 #endif
