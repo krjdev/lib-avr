3f855234a83c5049d7592ed7bf97e12db705123c
Johannes Krottmayer <krjdev@gmail.com>
2019-02-02 05:50:39 +0100

    add FIFO buffer library

---
+++ lib/fifo.h
@@ -0,0 +1,40 @@
+/**
+ *
+ * File Name: fifo.h
+ * Title    : FIFO buffer library header
+ * Project  : lib-avr
+ * Author   : Copyright (C) 2019 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2019-02-02
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see file LICENSE.txt)
+ * Target   : Atmel AVR Series
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#ifndef LIBAVR_LIB_FIFO_H
+#define LIBAVR_LIB_FIFO_H
+
+#include <stdint.h>
+
+typedef struct fifo {
+    int f_len;
+    int f_num;
+    uint8_t *f_p;
+    int f_idxr;
+    int f_idxw;
+} fifo_t;
+
+extern fifo_t *fifo_init(int len);
+extern void fifo_free(fifo_t *fifo);
+extern int fifo_enqueue(fifo_t *fifo, uint8_t data);
+extern int fifo_dequeue(fifo_t *fifo, uint8_t *data);
+extern int fifo_get_len(fifo_t *fifo);
+extern int fifo_get_num(fifo_t *fifo);
+
+#endif
