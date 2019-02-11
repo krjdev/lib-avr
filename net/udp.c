954348a4d54108de8382a9ceab06fe1fb3d394d7
Johannes Krottmayer <krjdev@gmail.com>
2019-02-11 08:48:54 +0100

    UDP lib: release version 0.2.0.0

--- net/udp.c
+++ net/udp.c
@@ -1,13 +1,13 @@
 /**
  *
  * File Name: udp.c
- * Title    : UDP definitions and helper functions source
+ * Title    : UDP library
  * Project  : lib-avr
  * Author   : Copyright (C) 2019 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2019-01-30
- * Modified : 
+ * Modified : 2019-02-11
  * Revised  : 
- * Version  : 0.1.0.0
+ * Version  : 0.2.0.0
  * License  : ISC (see file LICENSE.txt)
  * Target   : Atmel AVR Series
  *
@@ -17,76 +17,286 @@
  *
  */
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 #include "udp.h"
 
-int udp_set_port_src(udp_hdr_t *udp, uint16_t src)
+#define _HIGH16(val)    ((uint8_t) (((val) & 0xFF00) >> 8))
+#define _LOW16(val)     ((uint8_t) ((val) & 0x00FF))
+
+static uint32_t udp_sum(udp_packet_t *udp)
+{
+    int i = 0;
+    int j;
+    uint32_t sum = 0;
+    uint16_t tmp;
+    
+    if (!udp)
+        return 0;
+    
+    sum += udp->up_hdr.uh_srcp;
+    sum += udp->up_hdr.uh_dstp;
+    sum += udp->up_hdr.uh_len;
+    
+    if (udp->up_payload_len > 0) {
+        if (!udp->up_payload_buf)
+            return 0;
+        
+        for (j = 0; j < udp->up_payload_len / 2; j++ ) {
+            tmp = ((uint16_t) udp->up_payload_buf[i++] << 8);
+            tmp |= udp->up_payload_buf[i++];
+            sum += tmp;
+        }
+        
+        if (udp->up_payload_len & 1) {
+            tmp = ((uint16_t) udp->up_payload_buf[i] << 8);
+            sum += tmp;
+        }
+    }
+    
+    return sum;
+}
+
+int udp_pkt_set_srcp(udp_packet_t *udp, uint16_t srcp)
 {
     if (!udp)
         return -1;
     
-    udp->uh_srcp = src;
+    udp->up_hdr.uh_srcp = srcp;
     return 0;
 }
 
-int udp_set_port_dst(udp_hdr_t *udp, uint16_t dst)
+int udp_pkt_set_dstp(udp_packet_t *udp, uint16_t dstp)
 {
     if (!udp)
         return -1;
     
-    udp->uh_dstp = dst;
+    udp->up_hdr.uh_dstp = dstp;
     return 0;
 }
 
-int udp_set_len(udp_hdr_t *udp, uint16_t len)
+int udp_pkt_set_payload(udp_packet_t *udp, uint8_t *buf, int len)
 {
+    uint8_t *p;
+    
     if (!udp)
         return -1;
     
-    udp->uh_len = len;
+    if (!buf)
+        return -1;
+    
+    if (len < 1)
+        return -1;
+    
+    p = (uint8_t *) malloc(len);
+    
+    if (!p)
+        return -1;
+    
+    memcpy(p, buf, len);
+    udp->up_hdr.uh_len += len;
+    udp->up_payload_buf = p;
+    udp->up_payload_len = len;
     return 0;
 }
 
-int udp_set_chk(udp_hdr_t *udp, uint16_t chk)
+int udp_pkt_get_srcp(udp_packet_t *udp, uint16_t *srcp)
 {
     if (!udp)
         return -1;
     
-    udp->uh_chk = chk;
+    (*srcp) = udp->up_hdr.uh_srcp;
     return 0;
 }
 
-int udp_get_port_src(udp_hdr_t *udp, uint16_t *src)
+int udp_pkt_get_dstp(udp_packet_t *udp, uint16_t *dstp)
 {
     if (!udp)
         return -1;
     
-    (*src) = udp->uh_srcp;
+    (*dstp) = udp->up_hdr.uh_dstp;
     return 0;
 }
 
-int udp_get_port_dst(udp_hdr_t *udp, uint16_t *dst)
+int udp_pkt_get_payload_len(udp_packet_t *udp)
 {
     if (!udp)
         return -1;
     
-    (*dst) = udp->uh_dstp;
+    return udp->up_payload_len;
+}
+
+int udp_pkt_get_payload(udp_packet_t *udp, uint8_t **buf)
+{
+    if (!udp)
+        return -1;
+    
+    if (!buf)
+        return -1;
+    
+    (*buf) = udp->up_payload_buf;
     return 0;
 }
 
-int udp_get_len(udp_hdr_t *udp, uint16_t *len)
+int udp_pkt_get_len(udp_packet_t *udp)
 {
     if (!udp)
         return -1;
     
-    (*len) = udp->uh_len;
+    return udp->up_hdr.uh_len;
+}
+
+int udp_pkt_create(uint16_t srcp, uint16_t dstp, 
+                   uint8_t *buf, int len, 
+                   udp_packet_t *udp)
+{
+    if (!buf)
+        return -1;
+    
+    if (!udp)
+        return -1;
+    
+    udp->up_hdr.uh_srcp = srcp;
+    udp->up_hdr.uh_dstp = dstp;
+    udp->up_hdr.uh_len = 8;
+    udp_pkt_set_payload(udp, buf, len);
     return 0;
 }
 
-int udp_get_chk(udp_hdr_t *udp, uint16_t *chk)
+int udp_pkt_free(udp_packet_t *udp)
 {
     if (!udp)
         return -1;
     
-    (*chk) = udp->uh_chk;
+    if (udp->up_payload_len > 0) {
+        if (!udp->up_payload_buf)
+            return -1;
+        
+        free(udp->up_payload_buf);
+    }
+}
+
+int udp_pkt_to_ip(udp_packet_t *udp, ipv4_packet_t *ip_udp)
+{
+    uint32_t sum;
+    uint16_t tmp;
+    uint16_t carry;
+    uint8_t *p;
+    uint8_t *p_payl;
+    int i = 0;
+    
+    if (!udp)
+        return -1;
+    
+    if (!ip_udp)
+        return -1;
+    
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_src.ia_byte0 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_src.ia_byte1;
+    sum += tmp;
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_src.ia_byte2 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_src.ia_byte3;
+    sum += tmp;
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte0 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte1;
+    sum += tmp;
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte2 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte3;
+    sum += tmp;
+    tmp = ip_udp->ip_hdr.ih_prot;
+    sum += tmp;
+    sum += udp_sum(udp);
+    carry = (uint16_t) (sum >> 16);
+    sum += carry;
+    sum = ~sum;
+    udp->up_hdr.uh_chk = (uint16_t) sum;
+    p = (uint8_t *) malloc(udp->up_hdr.uh_len);
+    
+    if (!p)
+        return -1;
+    
+    p[i++] = _HIGH16(udp->up_hdr.uh_srcp);
+    p[i++] = _LOW16(udp->up_hdr.uh_srcp);
+    p[i++] = _HIGH16(udp->up_hdr.uh_dstp);
+    p[i++] = _LOW16(udp->up_hdr.uh_dstp);
+    p[i++] = _HIGH16(udp->up_hdr.uh_len);
+    p[i++] = _LOW16(udp->up_hdr.uh_len);
+    p[i++] = _HIGH16(udp->up_hdr.uh_chk);
+    p[i++] = _LOW16(udp->up_hdr.uh_chk);
+    
+    if (udp_pkt_get_payload(udp, &p_payl) == -1) {
+        free(p);
+        return -1;
+    }
+    
+    memcpy(&p[i], p_payl, udp_pkt_get_payload_len(udp));
+    
+    if (ipv4_pkt_set_payload(ip_udp, p, udp->up_hdr.uh_len) == -1) {
+        free(p);
+        return -1;
+    }
+    
+    free(p);
+    return 0;
+}
+
+int udp_ip_to_pkt(ipv4_packet_t *ip_udp, udp_packet_t *udp)
+{
+    uint8_t *p;
+    uint32_t sum = 0;
+    uint16_t tmp;
+    uint16_t carry;
+    uint16_t chk;
+    int i = 0;
+    int len;
+    
+    if (!ip_udp)
+        return -1;
+    
+    if (!udp)
+        return -1;
+    
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_src.ia_byte0 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_src.ia_byte1;
+    sum += tmp;
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_src.ia_byte2 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_src.ia_byte3;
+    sum += tmp;
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte0 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte1;
+    sum += tmp;
+    tmp = ((uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte2 << 8);
+    tmp |= (uint16_t) ip_udp->ip_hdr.ih_dst.ia_byte3;
+    sum += tmp;
+    tmp = ip_udp->ip_hdr.ih_prot;
+    sum += tmp;
+    len = ipv4_pkt_get_payload_len(ip_udp);
+    
+    if (len < 8)
+        return -1;
+    
+    ipv4_pkt_get_payload(ip_udp, &p);
+    udp->up_hdr.uh_srcp = ((uint16_t) p[i++] << 8);
+    udp->up_hdr.uh_srcp |= (uint16_t) p[i++];
+    udp->up_hdr.uh_dstp = ((uint16_t) p[i++] << 8);
+    udp->up_hdr.uh_dstp |= (uint16_t) p[i++];
+    udp->up_hdr.uh_len = ((uint16_t) p[i++] << 8);
+    udp->up_hdr.uh_len |= (uint16_t) p[i++];
+    udp->up_hdr.uh_chk = ((uint16_t) p[i++] << 8);
+    udp->up_hdr.uh_chk |= (uint16_t) p[i++];
+    udp_pkt_set_payload(udp, &p[i], len);
+    sum += udp_sum(udp);
+    carry = (uint16_t) (sum >> 16);
+    sum += carry;
+    sum = ~sum;
+    chk = (uint16_t) sum;
+    
+    if (udp->up_hdr.uh_chk != chk) {
+        return -1;
+        udp_pkt_free(udp);
+    }
+    
     return 0;
 }
--- net/udp.h
+++ net/udp.h
@@ -1,13 +1,13 @@
 /**
  *
  * File Name: udp.h
- * Title    : UDP definitions and helper functions header
+ * Title    : UDP library
  * Project  : lib-avr
  * Author   : Copyright (C) 2019 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2019-01-30
- * Modified : 
+ * Modified : 2019-02-11
  * Revised  : 
- * Version  : 0.1.0.0
+ * Version  : 0.2.0.0
  * License  : ISC (see file LICENSE.txt)
  * Target   : Atmel AVR Series
  *
@@ -22,6 +22,8 @@
 
 #include <stdint.h>
 
+#include "ipv4.h"
+
 typedef struct udp_hdr {
     uint16_t uh_srcp;   /* Source port */
     uint16_t uh_dstp;   /* Destination port */
@@ -29,13 +31,25 @@ typedef struct udp_hdr {
     uint16_t uh_chk;    /* Checksum */
 } udp_hdr_t;
 
-extern int udp_set_port_src(udp_hdr_t *udp, uint16_t src);
-extern int udp_set_port_dst(udp_hdr_t *udp, uint16_t dst);
-extern int udp_set_len(udp_hdr_t *udp, uint16_t len);
-extern int udp_set_chk(udp_hdr_t *udp, uint16_t chk);
-extern int udp_get_port_src(udp_hdr_t *udp, uint16_t *src);
-extern int udp_get_port_dst(udp_hdr_t *udp, uint16_t *dst);
-extern int udp_get_len(udp_hdr_t *udp, uint16_t *len);
-extern int udp_get_chk(udp_hdr_t *udp, uint16_t *chk);
+typedef struct udp_packet {
+    udp_hdr_t up_hdr;
+    uint8_t *up_payload_buf;
+    int up_payload_len;
+} udp_packet_t;
+
+extern int udp_pkt_set_srcp(udp_packet_t *udp, uint16_t srcp);
+extern int udp_pkt_set_dstp(udp_packet_t *udp, uint16_t dstp);
+extern int udp_pkt_set_payload(udp_packet_t *udp, uint8_t *buf, int len);
+extern int udp_pkt_get_srcp(udp_packet_t *udp, uint16_t *srcp);
+extern int udp_pkt_get_dstp(udp_packet_t *udp, uint16_t *dstp);
+extern int udp_pkt_get_payload_len(udp_packet_t *udp);
+extern int udp_pkt_get_payload(udp_packet_t *udp, uint8_t **buf);
+extern int udp_pkt_get_len(udp_packet_t *udp);
+extern int udp_pkt_create(uint16_t srcp, uint16_t dstp, 
+                          uint8_t *buf, int len, 
+                          udp_packet_t *udp);
+extern int udp_pkt_free(udp_packet_t *udp);
+extern int udp_pkt_to_ip(udp_packet_t *udp, ipv4_packet_t *ip_udp);
+extern int udp_ip_to_pkt(ipv4_packet_t *ip_udp, udp_packet_t *udp);
 
 #endif
